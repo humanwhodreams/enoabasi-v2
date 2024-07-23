7f4f0a9bb57bd26baa76f1c0a609fa5215afa5c4
enoabasi <watermelonbit@gmail.com>
2024-07-23 07:54:17 +0100

    feat: Add scroll to top + remove table of content
    - Add scroll to top for long contents
    - remove table of contents
    
    table of contents has a depth issue I need to look into using unist.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,12 +2,11 @@ import { getPostFromParams, getReadingTime } from "@/lib/v-functions";
 
 import { BlogNavigation } from "@/components/blog-navigation";
 import { MdxComponents } from "@/components/mdx-components";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollToTop } from "@/components/scroll-to-top";
 import { SectionContainer } from "@/components/section-container";
-import { TableOfContents } from "@/components/table-of-contents";
 import { formatDate } from "@/lib/date";
 import { notFound } from "next/navigation";
-import { posts } from "#vContent";
+import { type Post, posts } from "#vContent";
 
 interface Props {
   params: {
@@ -28,37 +27,34 @@ export default async function SpecificBlog({ params }: Props) {
     notFound();
   }
 
-  const readingTime = await getReadingTime(post.content);
-
   return (
     <SectionContainer className="relative">
-      <div className="hidden text-sm xl:block">
-        <div className="fixed top-[10rem] right-[6rem] -mt-10 pt-4 z-10">
-          <ScrollArea className="pb-10">
-            <div className="sticky top-[10rem] -mt-10 h-[calc(100vh-3.5rem)] py-12 px-4">
-              <TableOfContents toc={post.toc} />
-            </div>
-          </ScrollArea>
-        </div>
-      </div>
-
       <BlogNavigation pageTitle={post.title} />
-      <article className="font-sans prose lg:prose-lg dark:prose-invert">
-        <h1 className="text-xl font-bold md:text-3xl">{post.title}</h1>
-        {post.description ? (
-          <p className="before:content-['~_'] text-primary text-sm md:text-base md:font-medium">
-            {post.description}
-          </p>
-        ) : null}
-        <ul className="text-sm">
-          <li>Written by: {post.authors}</li>
-          <li>Posted: {formatDate(post.date)}</li>
-          <li>
-            Reading time: {readingTime} {readingTime >= 2 ? "minutes" : "minute"}
-          </li>
-        </ul>
-        <MdxComponents code={post.content} />
-      </article>
+      <Article post={post} />
+      <ScrollToTop />
     </SectionContainer>
+  );
+}
+
+async function Article({ post }: { post: Post }) {
+  const readingTime = await getReadingTime(post.content);
+  
+  return (
+    <article className="font-sans prose lg:prose-lg dark:prose-invert">
+      <h1 className="text-xl font-bold md:text-3xl">{post.title}</h1>
+      {post.description ? (
+        <p className="before:content-['~_'] text-primary text-sm md:text-base md:font-medium">
+          {post.description}
+        </p>
+      ) : null}
+      <ul className="text-sm">
+        <li>Written by: {post.authors}</li>
+        <li>Posted: {formatDate(post.date)}</li>
+        <li>
+          Reading time: {readingTime} {readingTime >= 2 ? "minutes" : "minute"}
+        </li>
+      </ul>
+      <MdxComponents code={post.content} />
+    </article>
   );
 }
